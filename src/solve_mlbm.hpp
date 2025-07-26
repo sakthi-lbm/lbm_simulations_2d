84932efbab221553d21927b43187096d27f5f02f
sakthi-lbm <sakthi.me12@gmail.com>
2025-07-26 10:49:32 -0300

    changed moments to structure of arrays

--- src/solve_mlbm.hpp
+++ src/solve_mlbm.hpp
@@ -4,10 +4,11 @@
 #include<iostream>
 
 #include "var.hpp"
+#include "global_struct.hpp"
 #include COLREC
 #include CASE_BOUNDARY
 
-void MomCollisionStreaming(dfloat *h_fMom, dfloat *f_in, dfloat *f_out);
-void calculate_moments(unsigned int *nodeType, dfloat *f_in, dfloat *h_fMom);
+void MomCollisionStreaming(nodeVar h_fMom);
+void calculate_moments(nodeVar h_fMom);
 
 #endif
